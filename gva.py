1d6332005b1cddab2ef02c6a5f9b5bf4d7726b34
Lode Nachtergaele <cast42@gmail.com>
2013-11-29 10:17:05 +0100

    Deal with the mobile links to gva.be

--- gva.py
+++ gva.py
@@ -48,14 +48,26 @@ def get_title_and_text(url):
 		print 'Could not get %s' % url
 		return ('no title found',"")
 	soup = BeautifulSoup(r.text)
-	try:
-		title = soup.find("meta", {"name":"title"})['content']
-		art_text = ''
-		for sibling in soup.find('span',attrs={"class":"date-marker"}).find_next_siblings():
-			art_text += sibling.text
-	except:
+	if url.startswith('http://www.gva.be/'):
+		try:
+			title = soup.find("meta", {"name":"title"})['content']
+			art_text = ''
+			for sibling in soup.find('span',attrs={"class":"date-marker"}).find_next_siblings():
+				art_text += sibling.text
+		except:
+			print 'no title or article text found for %s' % url
+			return ("no title found","")
+	elif url.startswith('http://m.gva.be/'):
+		try:
+			title = soup.find("meta", {"property":"og:title"})['content']
+			art_text = soup.find('div', attrs={'class':'txt'}).text
+		except:
+			print 'no title or article text found for %s' % url
+			return ("no title found","")
+	else:
 		print 'no title or article text found for %s' % url
 		return ("no title found","")
+
 	return (title, art_text)
 	
 # XXX: Go to http://dev.twitter.com/apps/new to create an app and get values
