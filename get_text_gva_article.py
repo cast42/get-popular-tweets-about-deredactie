dcf479bbb7b38a5d9ce092b0c3b470560b933f61
Lode Nachtergaele <cast42@gmail.com>
2013-11-30 11:47:26 +0100

    More pythonic expression to retrieve article text

--- get_text_gva_article.py
+++ get_text_gva_article.py
@@ -15,9 +15,8 @@ def get_title_and_text(url):
 	if url.startswith('http://www.gva.be/'):
 		try:
 			title = soup.find("meta", {"name":"title"})['content']
-			art_text = ''
-			for sibling in soup.find('span',attrs={"class":"date-marker"}).find_next_siblings():
-				art_text += sibling.text
+			art_text = ''.join([sibling.text for sibling in soup.find('span',attrs={"class":"date-marker"}).find_next_siblings()])
+
 		except:
 			print 'no title or article text found for %s' % url
 			return ("no title found","")
